eef6ce417419b08fae1961d159872370ac8f7b73
Nipendra Soni <rsoni2843@gmail.com>
2022-11-28 00:11:23 +0530

    day-2

---
+++ variables.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std ; 
+
+// endl is used to print something in next line. 
+
+int main(){
+    cout<<"Hello"<<endl<<"World" ; 
+    return 0 ; 
+//after return 0 next line code is not executed
+    cout<<"CHECK" ; 
+}
